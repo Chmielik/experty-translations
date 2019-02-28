f6f6bc75a326a1f36a98509b02eff26dd0c2be6a
Paweł Rokosz <rokosz.pawel@gmail.com>
2019-02-28 13:57:26 +0100

    add another doc

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const vars = {
-  SPREADSHEET: process.env.SPREADSHEET || '13ctq201NB77AQ5sRf0KMHTNBmxQTePT044Qnxh3IxTQ',
+  SPREADSHEET: process.env.SPREADSHEET || process.argv[2] === 'oldI18nSupport' ? '127lZtRjV5hqaYQ6twJMagsQmFnSRx0NSyLDayHXyNl4' : '13ctq201NB77AQ5sRf0KMHTNBmxQTePT044Qnxh3IxTQ'
 }
 
 module.exports = {
