cd14892491ab1afd12e80d437e8f68d5e4793525
Paweł Rokosz <rokosz.pawel@gmail.com>
2019-02-28 14:45:08 +0100

    add exportable method

--- index.js
+++ index.js
@@ -7,60 +7,66 @@ const en = 'en'
 const pl = 'pl'
 const languages = [en, pl]
 
-languages.forEach(lang => {
-  let dir
-  let path
-  if (process.argv[2] === 'oldI18nSupport') {
-    dir = `./locales`
-    path = process.cwd() + `/locales/${lang}.json`
-  } else {
-    dir = `./src/static/locales/${lang}`
-    path = process.cwd() + `/src/static/locales/${lang}/common.json`
-  }
-
-  // Check if './src/static/locales/${lang} dir exists
-  // Create dir if doesn't exist
-  if (!fs.existsSync(dir)) {
-    mkdirp(dir)
-  }
-
-  // Remove old translation files
-  try {
-    if (fs.existsSync(path)) {
-      fs.unlinkSync(path)
+const translate = () => {
+  languages.forEach(lang => {
+    let dir
+    let path
+    if (process.argv[2] === 'oldI18nSupport') {
+      dir = `./locales`
+      path = process.cwd() + `/locales/${lang}.json`
+    } else {
+      dir = `./src/static/locales/${lang}`
+      path = process.cwd() + `/src/static/locales/${lang}/common.json`
     }
-  } catch (e) {
-    throw e
-  }
-})
 
-https.get(`https://spreadsheets.google.com/feeds/list/${SPREADSHEET}/od6/public/values?alt=json`, (resp) => {
-  let data = ''
+    // Check if './src/static/locales/${lang} dir exists
+    // Create dir if doesn't exist
+    if (!fs.existsSync(dir)) {
+      mkdirp(dir)
+    }
 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk
+    // Remove old translation files
+    try {
+      if (fs.existsSync(path)) {
+        fs.unlinkSync(path)
+      }
+    } catch (e) {
+      throw e
+    }
   })
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    const feed = JSON.parse(data).feed.entry
-    languages.forEach(lang => {
-      let filePath = process.argv[2] === 'oldI18nSupport' ? `./locales/${lang}.json` : `./src/static/locales/${lang}/common.json`
+  https.get(`https://spreadsheets.google.com/feeds/list/${SPREADSHEET}/od6/public/values?alt=json`, (resp) => {
+    let data = ''
 
-      const obj = {}
-      feed.map(row => {
-        // if (row['gsx$key']['$t'] && !row[`gsx$${lang}`]['$t']) throw new Error(`Missing "${row['gsx$key']['$t']}" key translation for ${lang} language`)
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk
+    })
 
-        obj[row['gsx$key']['$t']] = row[`gsx$${lang}`]['$t']
-        return JSON.parse(JSON.stringify(obj))
-      })
-      // convert obj to json, and append to file
-      fs.appendFile(filePath, JSON.stringify(obj, null, 2), (err) => {
-        if (err) throw err
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      const feed = JSON.parse(data).feed.entry
+      languages.forEach(lang => {
+        let filePath = process.argv[2] === 'oldI18nSupport' ? `./locales/${lang}.json` : `./src/static/locales/${lang}/common.json`
+
+        const obj = {}
+        feed.map(row => {
+          // if (row['gsx$key']['$t'] && !row[`gsx$${lang}`]['$t']) throw new Error(`Missing "${row['gsx$key']['$t']}" key translation for ${lang} language`)
+
+          obj[row['gsx$key']['$t']] = row[`gsx$${lang}`]['$t']
+          return JSON.parse(JSON.stringify(obj))
+        })
+        // convert obj to json, and append to file
+        fs.appendFile(filePath, JSON.stringify(obj, null, 2), (err) => {
+          if (err) throw err
+        })
       })
     })
+  }).on('error', (err) => {
+    throw err
   })
-}).on('error', (err) => {
-  throw err
-})
+}
+
+module.exports = {
+  translate
+}
